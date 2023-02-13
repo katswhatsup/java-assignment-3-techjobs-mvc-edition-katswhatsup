b8557e657a42da0486d945bbeb9d4dd38851cbfd
katswhatsup <118704052+katswhatsup@users.noreply.github.com>
2023-02-13 16:37:46 -0600

    final

--- src/main/java/org/launchcode/techjobs/mvc/controllers/ListController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/ListController.java
@@ -3,6 +3,7 @@ package org.launchcode.techjobs.mvc.controllers;
 
 import org.launchcode.techjobs.mvc.models.Job;
 import org.launchcode.techjobs.mvc.models.JobData;
+import org.springframework.boot.autoconfigure.batch.BatchProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,6 +30,7 @@ public class ListController {
         columnChoices.put("positionType", "Position Type");
         columnChoices.put("coreCompetency", "Skill");
 
+        tableChoices.put("all", "View All");
         tableChoices.put("employer", JobData.getAllEmployers());
         tableChoices.put("location", JobData.getAllLocations());
         tableChoices.put("positionType", JobData.getAllPositionTypes());
